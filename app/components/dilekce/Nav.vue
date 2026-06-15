0467d39669994265d0072c06d72722ac33b4f1bd
Gokhan <gokhan@sylow.net>
2026-06-15 10:20:08 +0300

    fix(navigation): Remove Iletisim link from top navigation to get rid of horizantal navigation on mobile devices

--- app/components/dilekce/Nav.vue
+++ app/components/dilekce/Nav.vue
@@ -8,10 +8,6 @@
         <span class="yd-wm-dot" aria-hidden="true" />
       </NuxtLink>
       <nav class="yd-nav-right">
-        <NuxtLink to="/ornekler" class="yd-nav-link">
-          <span>Örnekler</span>
-        </NuxtLink>
-        <span class="yd-nav-sep" aria-hidden="true">|</span>
         <NuxtLink to="/olustur" class="yd-nav-link yd-nav-cta">
           <DilekceIcon name="sparkle" :size="15" />
           <span>Yeni dilekçe</span>
